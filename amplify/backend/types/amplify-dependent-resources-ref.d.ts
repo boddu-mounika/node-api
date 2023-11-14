8092228783cb08725c676aed06abb7dc9f7a0eb1
Mounika <mounika27boddu@gmail.com>
2023-11-14 11:43:58 -0600

    added api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api747c26ec": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "nodeapiac732700": {
       "AppClientID": "string",
@@ -8,6 +15,15 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "nodeapi6e355eb4": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- src/App.js
+++ src/App.js
@@ -8,14 +8,60 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
+import Amplify, { API } from "aws-amplify";
+import React, { useEffect, useState } from "react";
+
+const myAPI = "api747c26ec";
+const path = "/customer";
 
 function App({ signOut }) {
+  function getCustomer(e) {
+    let customerId = e.input;
+    API.get(myAPI, path + "/" + customerId)
+      .then((response) => {
+        console.log(response);
+        let newCustomers = [...customers];
+        newCustomers.push(response);
+        setCustomers(newCustomers);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const [input, setInput] = useState("");
+  const [customers, setCustomers] = useState([]);
   return (
     <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
+      <div className="App">
+        <h1>Super Simple React App</h1>
+        <div>
+          <input
+            placeholder="customer id"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+        <br />
+        <button onClick={() => getCustomer({ input })}>
+          Get Customer From Backend
+        </button>
+
+        <h2 style={{ visibility: customers.length > 0 ? "visible" : "hidden" }}>
+          Response
+        </h2>
+        {customers.map((thisCustomer, index) => {
+          return (
+            <div key={thisCustomer.customerId}>
+              <span>
+                <b>CustomerId:</b> {thisCustomer.customerId} -{" "}
+                <b>CustomerName</b>: {thisCustomer.customerName}
+              </span>
+            </div>
+          );
+        })}
+      </div>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
