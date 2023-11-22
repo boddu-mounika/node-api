2e30da7f94c17bf61f946a03a70bfe65af626281
Mounika <mounika27boddu@gmail.com>
2023-11-22 12:53:28 -0600

    changes

--- amplify/backend/function/nodeapi6e355eb4/src/index.js
+++ amplify/backend/function/nodeapi6e355eb4/src/index.js
@@ -2,10 +2,11 @@
 // Use the following code to retrieve configured secrets from SSM:
 
 const aws = require("aws-sdk");
+//const app = express();
+//const cors = require("cors");
+//app.use(cors());
 
-// Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
-// */
-exports.handler = async (event) => {
+exports.handler =async (event) => {
   const { Parameters } = await new aws.SSM()
     .getParameters({
       Names: ["DB_USERNAME", "DB_PASS"].map(
@@ -14,9 +15,10 @@ exports.handler = async (event) => {
       WithDecryption: true,
     })
     .promise();
+
   console.log(Parameters);
-  console.log("point-1");
-  const promise = new Promise(function (resolve, reject) {
+
+  const promise = new Promise((resolve, reject) => {
     let sql = require("mssql");
 
     const config = {
@@ -26,13 +28,12 @@ exports.handler = async (event) => {
       port: 1433,
       options: {
         database: "StevePoc",
-        encrypt:false
+        encrypt: false,
       },
     };
-    console.log("point-2");
+
     sql.connect(config, (err) => {
       if (err) {
-        console.log("point-3");
         reject(err);
       } else {
         const request = new sql.Request();
@@ -40,26 +41,117 @@ exports.handler = async (event) => {
         const query = "SELECT * FROM customer";
         request.query(query, (err, result) => {
           if (err) {
-            console.log("point-4");
             reject(err);
           } else {
-            console.log("point-5");
-            const response = {
-              statusCode: 200,
-              //  Uncomment below to enable CORS requests
-              headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*",
-              },
-              body: JSON.stringify(result),
-            };
-            resolve(response);
+            resolve(result);
           }
         });
       }
-    })
+    });
   });
 
-  console.log("point-6");
-  return promise;
+  try {
+    const result = await promise;
+    console.log("Step-1");
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+      },
+      body: JSON.stringify(result),
+    };
+    return response;
+  } catch(e) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+      },
+      body: JSON.stringify(e.message),
+    };
+  }
 };
+// Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+// */
+// exports.handler = async (event) => {
+//   const corsOptions = {
+//     origin: "*",
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   };
+//   const corsMiddleware = cors(corsOptions);
+//   const { Parameters } = await new aws.SSM()
+//     .getParameters({
+//       Names: ["DB_USERNAME", "DB_PASS"].map(
+//         (secretName) => process.env[secretName]
+//       ),
+//       WithDecryption: true,
+//     })
+//     .promise();
+//   console.log(Parameters);
+//   console.log("point-1");
+//   const promise = new Promise((resolve, reject) => {
+//     corsMiddleware(event, {}, () => {
+//       let sql = require("mssql");
+
+//       const config = {
+//         user: Parameters[1].Value,
+//         password: Parameters[0].Value,
+//         server: "pocdb.cbjhfsw0n963.us-east-2.rds.amazonaws.com",
+//         port: 1433,
+//         options: {
+//           database: "StevePoc",
+//           encrypt: false,
+//         },
+//       };
+//       console.log("point-2");
+//       sql.connect(config, (err) => {
+//         if (err) {
+//           console.log("point-3");
+//           reject(err);
+//         } else {
+//           const request = new sql.Request();
+
+//           const query = "SELECT * FROM customer";
+//           request.query(query, (err, result) => {
+//             if (err) {
+//               console.log("point-4");
+//               reject(err);
+//             } else {
+//               console.log("point-5");
+
+//               resolve(result);
+//             }
+//           });
+//         }
+//       });
+//     });
+//   });
+//   try {
+//     const result = await promise;
+//     console.log(result);
+
+//     console.log("point-6");
+//     const response = {
+//       statusCode: 200,
+//       //  Uncomment below to enable CORS requests
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Headers": "*",
+//       },
+//       body: JSON.stringify(result),
+//     };
+//     return response;
+//   } catch(e) {
+//     return {
+//       statusCode: 500,
+//       //  Uncomment below to enable CORS requests
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Headers": "*",
+//       },
+//       body: JSON.stringify(e.message),
+//     };
+//   }
+// };
