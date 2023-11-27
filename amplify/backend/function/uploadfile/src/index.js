0eb93ed8e27ec2c1a6ac7ac7b71f514aad0dcf01
Mounika <mounika27boddu@gmail.com>
2023-11-27 02:06:12 -0600

    ento

--- amplify/backend/function/uploadfile/src/index.js
+++ amplify/backend/function/uploadfile/src/index.js
@@ -1,37 +1,42 @@
-const PDFParser = require('pdf-parse');
+const PDFParser = require("pdf-parse");
 
 exports.handler = async (event) => {
   const { pdfData } = event.body;
-  const pdfBuffer = Buffer.from(pdfData, 'base64');
-    //console.log()
+  const pdfBuffer = Buffer.from(pdfData, "base64");
+  //console.log()
   try {
     const pdf = await PDFParser.pdf2json(pdfBuffer);
     const plainText = pdf.text; // Extract plain text from the PDF file
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*"
+        
+      },
       body: plainText,
     };
   } catch (error) {
     console.error(error);
     return {
       statusCode: 500,
-      body: 'Error parsing the PDF file.',
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*"
+        
+      },
+      body: "Error parsing the PDF file.",
     };
   }
 };
 
-
-
-
-
-
 // const pdfParse = require("pdf-parse");
 
 // exports.handler = async (event) => {
-//     // let base64string = event.body ; 
+//     // let base64string = event.body ;
 //     // let bufferObj = Buffer.from(base64string, "base64");
-//     // let decodedString = bufferObj.toString("utf8"); 
+//     // let decodedString = bufferObj.toString("utf8");
 //     // return{
 //     //     headers: {
 //     //         "Access-Control-Allow-Origin": "*",
@@ -39,11 +44,9 @@ exports.handler = async (event) => {
 //     //         "Content-Type":"multipart/form-data"
 //     //     },
 //     //     statusCode: 205,
-//     //     body:JSON.stringify(event)          
-//     // }; 
-    
-  
-   
+//     //     body:JSON.stringify(event)
+//     // };
+
 //     if (!event.files) {
 //         return {
 //             statusCode: 400,
@@ -53,8 +56,8 @@ exports.handler = async (event) => {
 //              "Access-Control-Allow-Headers": "*"
 //          },
 //             body: JSON.stringify("No file inpput"+result.text),
-            
-//         }; 
+
+//         };
 //       }
 //     pdfParse(event.files.pdfFile).then((result) => {
 //         return {
@@ -63,11 +66,11 @@ exports.handler = async (event) => {
 //          headers: {
 //              "Access-Control-Allow-Origin": "*",
 //              "Access-Control-Allow-Headers": "*"
-             
+
 //          },
 //             body: JSON.stringify(result.text),
 
-//         };      
+//         };
 //     }).catch((error)=>{
 //         return {
 //             statusCode: 400,
@@ -77,6 +80,6 @@ exports.handler = async (event) => {
 //              "Access-Control-Allow-Headers": "*"
 //          },
 //             body: error,
-//         }; 
+//         };
 //     });
 // };
